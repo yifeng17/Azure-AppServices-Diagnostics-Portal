59173f8da5f96c62197d9d48aa4ff4222f6986d4
stevenernst131 <sterns@microsoft.com>
2017-11-13 17:31:47 -0800

    Add Linux/Windows to all telemetry
    Moving the calls to get this information into logging.service, which
    means all the other logging services will enherit it

--- app/shared/services/logging/availability.logging.service.ts
+++ app/shared/services/logging/availability.logging.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
-import { LoggingService, PortalService, AuthService } from '../';
+import { LoggingService, PortalService, AuthService, ArmService } from '../';
 import { AvailabilityEventType } from './events.enumerations';
 import { SummaryHealthStatus } from '../../../shared/models/summary-view-model';
 
 @Injectable()
 export class AvailabilityLoggingService extends LoggingService {
 
-    constructor(private _portalService: PortalService, private _authService: AuthService) {
-        super(_portalService, _authService);
+    constructor(private _portalService: PortalService, private _authService: AuthService, private _armService: ArmService) {
+        super(_portalService, _authService, _armService);
     }
 
     LogAnalysisInitialized(analysisName, category: string = 'Availability') {
