f78af003930971d97117ff5da8d5f0a541f54471
stevenernst131 <sterns@microsoft.com>
2018-04-27 09:42:30 -0700

    Update beta subs

--- src/app/betaSubscriptions.ts
+++ src/app/betaSubscriptions.ts
@@ -9,6 +9,7 @@ export class DemoSubscriptions {
         "4adb32ad-8327-4cbb-b775-b84b4465bb38", // antps32
         "ef90e930-9d7f-4a60-8a99-748e0eea69de", // AntaresDemo
         "fb2c25dc-6bab-45c4-8cc9-cece7c42a95a", // antps14
-        "edcc99a4-b7f9-4b5e-a9a1-3034c51db496"  // OtherDemo?
+        "edcc99a4-b7f9-4b5e-a9a1-3034c51db496", // OtherDemo?
+        "ce678eba-8ab3-4a0c-9610-97fee4ee6b34"  //Ranjith linux test sub
     ];
 }
