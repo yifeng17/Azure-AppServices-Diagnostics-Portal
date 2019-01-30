37d0f55a67651d504075d00667afaa740b2c9f0e
Daniel Grooms <daniel.grooms@microsoft.com>
2019-01-30 14:07:15 -0800

    Pulled out namespace, need to revisit if anyone actually wants this

--- AngularApp/projects/app-service-diagnostics/src/app/shared/models/detectorresponse.ts
+++ AngularApp/projects/app-service-diagnostics/src/app/shared/models/detectorresponse.ts
@@ -3,53 +3,51 @@ import { IssueType } from './enumerations';
 import { INameValuePair } from './namevaluepair';
 import { ISolution } from './solution';
 
-namespace DetectorResponse {
-  export interface IDetectorResponse {
-      startTime: string;
-      endTime: string;
-      issueDetected: boolean;
-      detectorDefinition: IDetectorDefinition;
-      metrics: IMetricSet[];
-      abnormalTimePeriods: IDetectorAbnormalTimePeriod[];
-      data: INameValuePair[][];
-      responseMetaData: IDetectorMetaData;
-  }
+export interface IDetectorResponse {
+    startTime: string;
+    endTime: string;
+    issueDetected: boolean;
+    detectorDefinition: IDetectorDefinition;
+    metrics: IMetricSet[];
+    abnormalTimePeriods: IDetectorAbnormalTimePeriod[];
+    data: INameValuePair[][];
+    responseMetaData: IDetectorMetaData;
+}
 
-  export interface IMetricSet {
-      name: string;
-      unit: string;
-      startTime: string;
-      endTime: string;
-      timeGrain: string;
-      values: IMetricSample[];
-  }
+export interface IMetricSet {
+    name: string;
+    unit: string;
+    startTime: string;
+    endTime: string;
+    timeGrain: string;
+    values: IMetricSample[];
+}
 
-  export interface IMetricSample {
-      timestamp: string;
-      roleInstance: string;
-      total: number;
-      maximum: number;
-      minimum: number;
-      isAggregated: boolean;
-  }
+export interface IMetricSample {
+    timestamp: string;
+    roleInstance: string;
+    total: number;
+    maximum: number;
+    minimum: number;
+    isAggregated: boolean;
+}
 
-  export interface IDetectorAbnormalTimePeriod {
-      startTime: string;
-      endTime: string;
-      message: string;
-      source: string;
-      priority: number;
-      metaData: INameValuePair[][];
-      type: IssueType;
-      solutions: ISolution[];
-  }
+export interface IDetectorAbnormalTimePeriod {
+    startTime: string;
+    endTime: string;
+    message: string;
+    source: string;
+    priority: number;
+    metaData: INameValuePair[][];
+    type: IssueType;
+    solutions: ISolution[];
+}
 
-  export interface IDetectorMetaData {
-      dataSource: IDataSource;
-  }
+export interface IDetectorMetaData {
+    dataSource: IDataSource;
+}
 
-  export interface IDataSource {
-      instructions: string[];
-      dataSourceUri: INameValuePair[][];
-  }
+export interface IDataSource {
+    instructions: string[];
+    dataSourceUri: INameValuePair[][];
 }
