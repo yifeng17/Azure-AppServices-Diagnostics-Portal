31d315e9fb99ae8c32c9b3e4ea8df3362448bb57
nmallick1 <nmallick@microsoft.com>
2019-02-04 13:15:05 -0800

    Removed duplicate code from guage.ts constructor for initialization

--- AngularApp/projects/diagnostic-data/src/lib/models/guage.ts
+++ AngularApp/projects/diagnostic-data/src/lib/models/guage.ts
@@ -106,59 +106,7 @@ export class GuageGraphic{
 	constructor(fillColor : string, percentFilled : number, numberDisplay:string, label:string, size : GuageSize ) 
 	{
 		this.turnMultiplicationFactor = 0.005;
-		
-		if(fillColor.trim() == "")
-			this.fillColor = "#3a9bc7";
-		else
-			this.fillColor = fillColor;
-		
-	
-		if(percentFilled>100)
-		{
-			this.percentFilled = 100;
-			this.turnValue = Math.round(this.turnMultiplicationFactor * this.percentFilled * 1000)/1000; //This will round off the number to 3 decimal places		
-		}
-		else
-		{
-			if(percentFilled<1)
-			{
-				this.percentFilled = 0;
-				this.turnValue = 0.003; /*Ideally the turn should be 0 however to make sure that the needle is visible, turn it just a little*/
-			}
-			else
-			{
-				this.percentFilled = Math.round(percentFilled);
-				this.turnValue = Math.round(this.turnMultiplicationFactor * this.percentFilled * 1000)/1000; //This will round off the number to 3 decimal places
-			}
-		}
-				
-		
-		this.transformString = "rotate(" + this.turnValue.toString() + "turn)";
-		
-		this.numberDisplay = numberDisplay;
-		this.label = label;
-		this.size = size;
-		//if(size == "SMALL")
-		if(size == GuageSize.Small)
-		{
-			this.width = 120;
-			this.height = 75;
-		}
-		else
-		{
-			//if(size == "LARGE")
-			if(size == GuageSize.Large)
-			{
-				this.width = 300;
-				this.height = 188;
-			}
-			else
-			{
-                //Default to Medium size
-				this.width = 200;
-				this.height = 125;
-			}
-		}
+		this.setGuageParameters(fillColor, percentFilled, numberDisplay, label, size );				
 	}
 }
 
