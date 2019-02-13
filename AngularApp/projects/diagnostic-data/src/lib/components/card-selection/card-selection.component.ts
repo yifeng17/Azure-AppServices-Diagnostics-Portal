3a4c538bbc419d545bfe9855419151e9edfc469b
Puneet Gupta <puneetg@microsoft.com>
2019-02-13 13:03:03 -0800

    Removing Icon and Adding color to Card Tiles (#227)
    * Removing Icon and Adding color to Card Tiles
    
    * fixin getColor method

--- AngularApp/projects/diagnostic-data/src/lib/components/card-selection/card-selection.component.ts
+++ AngularApp/projects/diagnostic-data/src/lib/components/card-selection/card-selection.component.ts
@@ -14,10 +14,9 @@ export class CardSelection {
   linkValue: string;
 }
 
-export enum CardActionType
-{
-    Detector,
-    Tool
+export enum CardActionType {
+  Detector,
+  Tool
 }
 
 @Component({
@@ -28,8 +27,9 @@ export enum CardActionType
 export class CardSelectionComponent extends DataRenderBaseComponent {
 
   cardSelections: CardSelection[] = [];
+  colors: string[] = ['rgb(186, 211, 245)', 'rgb(249, 213, 180)', 'rgb(208, 228, 176)', 'rgb(208, 175, 239)', 'rgb(170, 192, 208)', 'rgb(208, 170, 193)', 'rgb(166, 216, 209)', 'rgb(207, 217, 246)'];
 
-  constructor(private _diagnosticService: DiagnosticService, private _router: Router, 
+  constructor(private _diagnosticService: DiagnosticService, private _router: Router,
     private _activatedRoute: ActivatedRoute, protected telemetryService: TelemetryService, private _navigator: FeatureNavigationService) {
     super(telemetryService);
   }
@@ -52,5 +52,9 @@ export class CardSelectionComponent extends DataRenderBaseComponent {
     if (card && card.linkType === CardActionType.Detector) {
       this._navigator.NavigateToDetector(this._activatedRoute.snapshot.params['detector'], card.linkValue);
     }
+  }
+
+  public getColor(index: number): string {
+    return this.colors[index % this.colors.length];
   }
 }
