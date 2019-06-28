dde8fd276306aa6301405fc590f22d860b0ec7a3
nmallick1 <nmallick@microsoft.com>
2019-06-28 13:34:26 -0700

    Updating the anonymizer regex for password.

--- ApplensBackend/Services/FreshChatClientService/FreshChatClientService.cs
+++ ApplensBackend/Services/FreshChatClientService/FreshChatClientService.cs
@@ -276,7 +276,7 @@ namespace AppLensV3.Services
         {
             // (?<=(\bpass\b)|(\bpwd\b)|(\bpassword\b)|(\buserpass\b)) Pattern should start with either pass or pwd or password or userpass.
             // [^\w\r\n] Followed by at least one character including space but not the newline character.
-            // \b[\S]+ Match an entire word that contains any character and there should be at least one character present
+            // (.+) Match everything and there should be at least one character present
 
             /* Matches
              * pass abc@#_!(?%123 
@@ -291,7 +291,7 @@ namespace AppLensV3.Services
              * Pass:
              * I am doing fine :)
              */
-            string pattern = @"(?<=(\bpass\b)|(\bpwd\b)|(\bpassword\b)|(\buserpass\b))[^\w\r\n]+\b[\S]+";
+            string pattern = @"(?<=(\bpass\b)|(\bpwd\b)|(\bpassword\b)|(\buserpass\b))[^\w\r\n]+(.+)";
             return Regex.Replace(content, pattern, m => ":****", RegexOptions.IgnoreCase);
         }
 
