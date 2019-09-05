ef432c0edf7932052b806872781b0d0653ca13b9
Puneet Gupta <puneetg@microsoft.com>
2019-09-05 15:34:31 +0530

    ngx-datatable changes for EventViewer (#483)
    * ngx-datatable changes for EventViewer
    
    * Adding check for allowColumnSearch

--- AngularApp/projects/diagnostic-data/src/lib/components/data-table/data-table.component.ts
+++ AngularApp/projects/diagnostic-data/src/lib/components/data-table/data-table.component.ts
@@ -1,22 +1,52 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, TemplateRef } from '@angular/core';
 import { DiagnosticData, DataTableRendering, RenderingType } from '../../models/detector';
 import { DataRenderBaseComponent } from '../data-render-base/data-render-base.component';
+import { DatatableComponent, SelectionType } from '@swimlane/ngx-datatable';
 
 @Component({
   selector: 'data-table',
   templateUrl: './data-table.component.html',
   styleUrls: ['./data-table.component.scss']
 })
-export class DataTableComponent extends DataRenderBaseComponent {
+export class DataTableComponent extends DataRenderBaseComponent implements AfterViewInit {
+
+  ngAfterViewInit(): void {
+
+    if (this.renderingProperties.descriptionColumnName != null) {
+      this.table.selectionType = SelectionType.single;
+      this.table.scrollbarV = true;
+      this.table.scrollbarH = false;
+      this.table.rowHeight = 35;
+      this.currentStyles = { 'height': '300px' };
+    }
+
+    if (this.renderingProperties.height != null && this.renderingProperties.height !== "") {
+      this.currentStyles = { 'height': this.renderingProperties.height };
+    }
+
+    if (this.renderingProperties.tableOptions != null) {
+      Object.keys(this.renderingProperties.tableOptions).forEach(item => {
+        this.table[item] = this.renderingProperties.tableOptions[item];
+      });
+    }
+
+    this.table.rows = this.rows;
+    this.table.columns = this.columns;
+  }
 
   DataRenderingType = RenderingType.Table;
   columns: any[];
+  selected = [];
   rows: any[];
+  rowsClone: any[];
   grouped: boolean = true;
   rowLimit: 25;
   renderingProperties: DataTableRendering;
+  currentStyles = {};
+  searchTexts = {};
 
-  @ViewChild('myTable') table: any;
+  @ViewChild('myTable') table: DatatableComponent
+  @ViewChild("headerTemplate") headerTemplate: TemplateRef<any>;
 
   protected processData(data: DiagnosticData) {
     super.processData(data);
@@ -25,13 +55,27 @@ export class DataTableComponent extends DataRenderBaseComponent {
   }
 
   private createNgxDataTableObjects() {
-    this.columns = this.diagnosticData.table.columns.map(column =>
+
+    let columns = this.diagnosticData.table.columns.map(column =>
       <any>{
         name: column.columnName,
         resizable: true,
         sortable: true,
         prop: column.columnName
       });
+
+    if (this.renderingProperties.allowColumnSearch) {
+      columns = this.diagnosticData.table.columns.map(column =>
+        <any>{
+          name: column.columnName,
+          resizable: true,
+          sortable: true,
+          prop: column.columnName,
+          headerTemplate: this.headerTemplate
+        });
+    }
+
+    this.columns = columns.filter((item) => item.name !== this.renderingProperties.descriptionColumnName);
     this.rows = [];
 
     this.diagnosticData.table.rows.forEach(row => {
@@ -42,11 +86,39 @@ export class DataTableComponent extends DataRenderBaseComponent {
       }
 
       this.rows.push(rowObject);
+
+      this.rowsClone = Object.assign([], this.rows);
     });
   }
 
   toggleExpandGroup(group) {
     this.table.groupHeader.toggleExpandGroup(group);
+  }
+
+  getValue(): any {
+    if (this.selected.length > 0) {
+      return this.selected[0][this.renderingProperties.descriptionColumnName];
+    }
+  }
+
+  updateFilter(event: any, prop: any) {
+    this.selected = [];
+    const val = event.target.value.toLowerCase();
+    this.searchTexts[prop] = val;
+
+    const temp = this.rowsClone.filter(item => {
+      let allMatch = true;
+      Object.keys(this.searchTexts).forEach(key => {
+        if (item[key]) {
+          allMatch = allMatch && item[key].toString().toLowerCase().indexOf(this.searchTexts[key]) !== -1;
+        }
+      });
+      return allMatch;
+    });
+
+    this.rows = temp;
+    this.table.rows = this.rows;
+
   }
 
 }
