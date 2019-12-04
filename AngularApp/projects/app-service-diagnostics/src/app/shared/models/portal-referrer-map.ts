2f315f1925085b8501b09b3807832c0762cd1485
nmallick1 <nmallick@microsoft.com>
2019-12-03 16:15:00 -0800

    Model for intercepting the values with Ibiza referrer information.

---
+++ AngularApp/projects/app-service-diagnostics/src/app/shared/models/portal-referrer-map.ts
@@ -0,0 +1,15 @@
+import { DetectorType } from "diagnostic-data";
+
+export interface PortalReferrerMap {
+    ReferrerExtensionName:string,
+    ReferrerBladeName: string,
+    ReferrerTabName: string,
+    DetectorType: DetectorType,
+    DetectorId: string
+}
+
+export interface PortalReferrerInfo {
+    ExtensionName:string,
+    BladeName:string,
+    TabName:string
+}
