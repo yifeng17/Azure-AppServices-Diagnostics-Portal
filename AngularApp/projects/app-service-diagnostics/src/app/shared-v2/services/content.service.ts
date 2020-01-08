8dc62dee9b519f23c3deee6264d0fd57d6ba3b83
Cindy Peng <xipeng@microsoft.com>
2020-01-07 18:02:50 -0800

    reset content service

--- AngularApp/projects/app-service-diagnostics/src/app/shared-v2/services/content.service.ts
+++ AngularApp/projects/app-service-diagnostics/src/app/shared-v2/services/content.service.ts
@@ -1,10 +1,11 @@
 
-import {map,  mergeMap, tap } from 'rxjs/operators';
+import {map} from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 import { Observable, of, BehaviorSubject } from 'rxjs';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Http, Headers } from '@angular/http';
 import { ResourceService } from './resource.service';
 import { BackendCtrlService } from '../../shared/services/backend-ctrl.service';
+import { mergeMap, tap } from 'rxjs/operators';
 
 @Injectable()
 export class ContentService {
@@ -22,16 +23,15 @@ export class ContentService {
     // }
   ];
 
-  private ocpApimKeyBehaviorSubject: BehaviorSubject<string> = new BehaviorSubject<string>('225eeab6c54e469189e3b6a1b5d00c1a');
-  private ocpApimKey: string = '225eeab6c54e469189e3b6a1b5d00c1a';
+  private ocpApimKeyBehaviorSubject: BehaviorSubject<string> = new BehaviorSubject<string>('');
+  private ocpApimKey: string = '';
 
-  constructor(private _http: HttpClient, private _resourceService: ResourceService, private _backendApi: BackendCtrlService) {
+  constructor(private _http: Http, private _resourceService: ResourceService, private _backendApi: BackendCtrlService) {
 
-    // this._backendApi.get<string>(`api/appsettings/ContentSearch:Ocp-Apim-Subscription-Key`).subscribe((value: string) =>{
-    //   this.ocpApimKeyBehaviorSubject.next(value);
-    //   this.ocpApimKey = value;
-    // });
-
+    this._backendApi.get<string>(`api/appsettings/ContentSearch:Ocp-Apim-Subscription-Key`).subscribe((value: string) =>{
+      this.ocpApimKeyBehaviorSubject.next(value);
+      this.ocpApimKey = value;
+    });
   }
 
   getContent(searchString?: string): Observable<any[]> {
@@ -49,23 +49,19 @@ export class ContentService {
     const query = encodeURIComponent(`${questionString} AND ${searchSuffix}`);
     const url = `https://api.cognitive.microsoft.com/bing/v7.0/search?q='${query}'&count=${resultsCount}`;
 
-
-    return this._http.get(url, { headers: {'Content-Type': 'application/json', 'Ocp-Apim-Subscription-Key': this.ocpApimKey}});
-
-    // return this.ocpApimKeyBehaviorSubject.pipe(
-    //   mergeMap((key:string)=>{
-    //     return this._http.get(url, { headers: this.getWebSearchHeaders() }).pipe(map(response => response));
-    //   })
-    // );
+    return this.ocpApimKeyBehaviorSubject.pipe(
+      mergeMap((key:string)=>{
+        return this._http.get(url, { headers: this.getWebSearchHeaders() }).pipe(map(response => response.json()));
+      })
+    );
   }
 
 
-  private getWebSearchHeaders(): HttpHeaders {
-    const headers = new HttpHeaders();
-    headers.set('Content-Type', 'application/json');
-    headers.set('Ocp-Apim-Subscription-Key', this.ocpApimKey);
-    console.log("apiheaders", this.ocpApimKey, headers);
-    console.log("headersvalue", headers.keys);
+  private getWebSearchHeaders(): Headers {
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    headers.append('Ocp-Apim-Subscription-Key', this.ocpApimKey);
+
     return headers;
   }
 
