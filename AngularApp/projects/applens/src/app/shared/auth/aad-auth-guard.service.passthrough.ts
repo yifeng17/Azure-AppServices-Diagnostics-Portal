1cd8837f36cb2e137f2dd6439deca2b601edaf30
SharmaAjay19 <SharmaAjay19@users.noreply.github.com>
2020-02-11 11:03:34 -0800

    Update passthrough auth guard to fix national cloud build (#622)

--- AngularApp/projects/applens/src/app/shared/auth/aad-auth-guard.service.passthrough.ts
+++ AngularApp/projects/applens/src/app/shared/auth/aad-auth-guard.service.passthrough.ts
@@ -4,6 +4,8 @@ import { Observable } from "rxjs";
 
 @Injectable()
 export class AadAuthGuard implements CanActivate {
+    public isTemporaryAccess: Boolean = false;
+    public temporaryAccessExpiryDays: number = 0;
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> {
         return true;
