a6e96507790c805210d2b6b98872df8c63b9b5be
rekhaswaminathan <46545195+rekhaswaminathan@users.noreply.github.com>
2020-03-10 13:58:12 -0700

    Adding more subs (#666)

--- AngularApp/projects/app-service-diagnostics/src/app/betaSubscriptions.ts
+++ AngularApp/projects/app-service-diagnostics/src/app/betaSubscriptions.ts
@@ -14,5 +14,18 @@ export class DemoSubscriptions {
         '88c8fe3d-1993-4fac-8e44-0f3232cc60ce',   // antps29,
         'da511dea-6e00-4728-93ff-6302ad7fe284',   // Nazeef's Sub
         '15c0f6ba-acb7-4f9a-aed0-7249c140b4f3', //Finbar' sub
+        'c1972e9d-b3c7-4de4-acb3-681773b28ced', // Cindy's sub: Private Test Sub Xipeng,
+        'caae90fd-95a3-4641-a523-b66894f4654c', // Xiaoxu's sub
+        'c258f9c0-3d64-4761-8697-cab631f28422', // Private Test Sub KHZAYED (Khaled's subscription)
+        'db8a84f3-059b-4491-9441-fb430ed8a187', // Private Test Sub YUNJCHOI (Yun Jung's subscription)
+        '00b9f3f9-5558-4bd8-9c4e-9ed47b92070e', //AppService-Marketplace-Subscription SUMUTH (Sunitha Subscription)
+        '72383ac7-d6f4-4a5e-bf56-b172f2fdafb2', // Private Test Sub RESWAMIN (Rekha's subscription)
+        '076fbf3e-ef06-46f2-af7e-76aa9f600612', // Private Test Sub DAGROOMS (Daniel's subscription)
+        'da511dea-6e00-4728-93ff-6302ad7fe284', //Microsoft Azure Internal Consumption (Nazeef's subscription)
+        '931906d8-00b9-481b-aba2-f227b237cada', //Private Test Sub NMALLICK (Nazeef's subscription)
+        '1402be24-4f35-4ab7-a212-2cd496ebdf14', // Antps05
+        '5abde51d-cc72-4bcc-b0d7-3c86b4db2a7c', // Shekhar's subscription
+        '6a205b0a-6b5a-4346-8468-f4b6367f7efa', //Private Test Sub HAWFOR (Hawk's subscription)
+        '6b6db65f-680e-4650-b97d-e82ed6a0f583', //Private Test Sub PUNEETG (Puneet's subscription)
     ];
 }
