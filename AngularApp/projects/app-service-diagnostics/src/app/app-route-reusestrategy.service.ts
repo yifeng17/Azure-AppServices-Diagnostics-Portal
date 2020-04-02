aff2f66a5bd18a8b94328b4dcc38dfba0ed5c954
nmallick1 <nmallick@microsoft.com>
2020-04-01 17:43:04 -0700

    Update angular routing strategy to not reuse cached components for downtime selections.

--- AngularApp/projects/app-service-diagnostics/src/app/app-route-reusestrategy.service.ts
+++ AngularApp/projects/app-service-diagnostics/src/app/app-route-reusestrategy.service.ts
@@ -5,19 +5,32 @@ import { Injectable, ComponentRef } from '@angular/core';
 export class CustomReuseStrategy implements RouteReuseStrategy {
 
     handlers: { [key: string]: DetachedRouteHandle } = {};
-
+    enableConsoleLogging:boolean = false;
     closedTab: string;
+
+    consoleLog(logString:string) {
+        if(this.enableConsoleLogging){
+            console.log(logString);
+        }
+    }
 
     /**
     * Determines if this route (and its subtree) should be detached to be reused later.
     */
     shouldDetach(route: ActivatedRouteSnapshot): boolean {
         const url = this._getUrl(route);
+        let res:boolean;
+        
         if (!route.routeConfig) {
-         return false; }
+            res = false; }
         if (route.routeConfig.loadChildren) {
-        return false; }
-        var res = !!route.data && !!(route.data as any).cacheComponent;
+            res = false; }
+        res = !!route.data && !!(route.data as any).cacheComponent;
+
+        this.consoleLog(`ShouldDetach... : ${url}`);
+        let cacheKey = this.getCacheKey(route, url); //Only for logging
+        this.consoleLog(`   Return Value : ${res}`);
+
         return res;
     }
 
@@ -27,13 +40,20 @@ export class CustomReuseStrategy implements RouteReuseStrategy {
     store(route: ActivatedRouteSnapshot, handle: DetachedRouteHandle): void {
         const url = this._getUrl(route);
 
-        if (this.closedTab === url) {
+        this.consoleLog(`Store...  ${url}`);
+
+        let cacheKey:string = this.getCacheKey(route, url);
+        
+        if (this.closedTab === url || this.closedTab === cacheKey) {
             this._deactivateOutlet(handle);
             this.closedTab = null;
+
+            this.consoleLog('   Closing & deactivating this...');
+
             return;
         }
 
-        this.handlers[url] = handle;
+        this.handlers[cacheKey] = handle;
     }
 
     /**
@@ -41,7 +61,11 @@ export class CustomReuseStrategy implements RouteReuseStrategy {
      */
     shouldAttach(route: ActivatedRouteSnapshot): boolean {
         const url = this._getUrl(route);
-        return !!route.routeConfig && !!this.handlers[url];
+        this.consoleLog(`ShouldAttach... : ${url}`);
+        
+        let res:boolean = !!route.routeConfig && !!this.handlers[this.getCacheKey(route, url)];
+        this.consoleLog(`   Return Value : ${res}`);
+        return res;
     }
 
     /**
@@ -49,34 +73,63 @@ export class CustomReuseStrategy implements RouteReuseStrategy {
      */
     retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle {
 
-        if (!route.routeConfig) {
-        return null; }
-        if (route.routeConfig.loadChildren) {
-        return null; }
-
         const url = this._getUrl(route);
-        return this.handlers[url];
+        this.consoleLog(`Retrieve... : ${url}`);
+
+        if (!route.routeConfig) {
+            this.consoleLog('   Return NULL. No route config');
+            return null;
+        }
+        if (route.routeConfig.loadChildren) {
+            this.consoleLog('   Return NULL. Load Children');
+            return null;
+        }
+
+        this.consoleLog('   Return component handle');
+
+        return this.handlers[this.getCacheKey(route, url)];
     }
 
     /**
      * Determines if a route should be reused.
      */
     shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
+        let res:boolean;
         if (curr.routeConfig === null && future.routeConfig === null) {
-            return true;
+            res = true;
         }
         if (future.routeConfig !== curr.routeConfig) {
-            return false;
+            res = false;
         }
 
-        return this._getUrl(future) === this._getUrl(curr);
+        res = this._getUrl(future) === this._getUrl(curr);
+        
+        this.consoleLog(`ShouldReuseRoute...`);
+        this.consoleLog(`   Future:  ${this._getUrl(future)}`);
+        let futureCacheKey = this.getCacheKey(future, this._getUrl(future));
+        this.consoleLog(`   Current:  ${this._getUrl(curr)}`);
+        let currentCacheKey = this.getCacheKey(curr, this._getUrl(curr));
+        this.consoleLog(`   Return Value : ${res}`);
+        return res;
 
     }
 
     removeCachedRoute(url: string) {
         this.closedTab = url;
         this._deactivateOutlet(this.handlers[url]);
+        this.consoleLog(`RemoveCachedRoute... : ${url}`);
         this.handlers[url] = null;
+    }
+
+    public getCacheKey(route: ActivatedRouteSnapshot, url:string):string {
+        let cacheKey:string = url;
+        let allRouteQueryParams = route.queryParams;
+        Object.keys(allRouteQueryParams).forEach(key => {
+                cacheKey += `&${key}=${encodeURIComponent(allRouteQueryParams[key])}`;
+        });
+        this.consoleLog(`   ---------------------------${cacheKey}`);
+
+        return cacheKey;
     }
 
     private _deactivateOutlet(handle: DetachedRouteHandle): void {
