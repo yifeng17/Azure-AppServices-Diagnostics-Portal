200c3c83cc694d9d5e0df944d2273c669e0feec1
Puneet Gupta <puneetg@microsoft.com>
2020-04-13 22:32:26 +0530

    Slight changes to Datasources tab (#708)

--- AngularApp/projects/applens/src/app/modules/dashboard/tabs/tab-data-sources/tab-data-sources.component.ts
+++ AngularApp/projects/applens/src/app/modules/dashboard/tabs/tab-data-sources/tab-data-sources.component.ts
@@ -15,11 +15,17 @@ export class TabDataSourcesComponent {
   }
 
   @Input() onboardingMode: boolean = false;
-  @Input () detectorResponse: DetectorResponse;
+  @Input() detectorResponse: DetectorResponse;
 
   detector: string;
   error: any;
   loadingDetector: boolean = true;
+  dataProviderMetadata: DataProviderMetadata[] = [];
+  providerColors = [{ name: "Kusto", color: "#2f2c69", text:"Run in Kusto Web Explorer" },
+  { name: "MDM", color: "#095fb0", text:"Jarvis link" },
+  { name: "AppInsights", color: "#68217a", text:"AppInights Log Analytics Documentation" },
+  { name: "AzureSupportCenter", color: "#0078d7", text:"Azure Support Center link" }
+  ];
 
   ngOnInit() {
 
@@ -32,7 +38,7 @@ export class TabDataSourcesComponent {
         this.getDetectorResponse();
       });
     }
-    else{
+    else {
       this.loadingDetector = false;
     }
   }
@@ -50,16 +56,63 @@ export class TabDataSourcesComponent {
     let additionalQueryString = '';
     let knownQueryParams = ['startTime', 'endTime'];
     Object.keys(allRouteQueryParams).forEach(key => {
-      if(knownQueryParams.indexOf(key) < 0) {
-          additionalQueryString += `&${key}=${encodeURIComponent(allRouteQueryParams[key])}`;
+      if (knownQueryParams.indexOf(key) < 0) {
+        additionalQueryString += `&${key}=${encodeURIComponent(allRouteQueryParams[key])}`;
       }
     });
     this._diagnosticApiService.getDetector(this.detector, this._detectorControlService.startTimeString, this._detectorControlService.endTimeString, this._detectorControlService.shouldRefresh, this._detectorControlService.isInternalView, additionalQueryString)
       .subscribe((response: DetectorResponse) => {
         this.loadingDetector = false;
         this.detectorResponse = response;
+        this.dataProviderMetadata = this.getDataProviderMetadata(response);
       }, (error: any) => {
         this.error = error;
       });
+  }
+
+  getDataProviderMetadata(response: DetectorResponse): DataProviderMetadata[] {
+    let metadata: DataProviderMetadata[] = [];
+    response.dataProvidersMetadata.forEach(element => {
+      if (this.hasMetadata(element)) {
+        let idx = metadata.findIndex(x => x.providerName.toLowerCase() === element.providerName.toLowerCase());
+        if (idx > -1) {
+          element.propertyBag.forEach(bag => {
+            if (bag.key != null && bag.key.length > 0) {
+              metadata[idx].propertyBag.push(bag);
+            }
+          });
+        } else {
+          metadata.push(element);
+        }
+      }
+    });
+
+    return metadata;
+  }
+
+  hasMetadata(providerMetadata: DataProviderMetadata): boolean {
+    return providerMetadata.propertyBag.length > 0 && providerMetadata.propertyBag.findIndex(x => x.key.length > 0) > -1;
+  }
+
+  getImageForProvider(providerName: string) {
+    return `assets/img/${providerName.toLowerCase()}.png`;
+  }
+
+  getColorForProvider(providerName: string) {
+    let idx = this.providerColors.findIndex(x => x.name.toLowerCase() === providerName.toLowerCase());
+    if (idx > -1) {
+      return this.providerColors[idx].color
+    } else {
+      return "lightgray";
+    }
+  }
+
+  getProviderText(providerName: string) {
+    let idx = this.providerColors.findIndex(x => x.name.toLowerCase() === providerName.toLowerCase());
+    if (idx > -1) {
+      return this.providerColors[idx].text
+    } else {
+      return "Link";
+    }
   }
 }
