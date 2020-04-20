9a34f6e3715fcd19fa0306ad805c70066c9bdb43
Rekha Swaminathan <reswamin@microsoft.com>
2020-04-20 11:18:42 -0700

    Point traffic diversion back to liberation.

--- AngularApp/projects/app-service-diagnostics/src/app/shared/utilities/versioningHelper.ts
+++ AngularApp/projects/app-service-diagnostics/src/app/shared/utilities/versioningHelper.ts
@@ -11,10 +11,9 @@ export class VersioningHelper {
         if (isBetaSubscription) {
             return true;
         }
-        return false;
-        // let firstDigit = "0x" + subscriptionId.substr(0, 1);
+        let firstDigit = "0x" + subscriptionId.substr(0, 1);
 
-        // // roughly split of 10% of subscriptions to use new feature.
-        // return (parseInt(firstDigit, 16) > 13) && enableV2;
+        // roughly split of 10% of subscriptions to use new feature.
+        return (parseInt(firstDigit, 16) > 13) && enableV2;
     }
 }
