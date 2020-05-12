8575b54c5b3b54d9be7d85a269fc2e5a00c5bbf7
nmallick1 <nmallick@microsoft.com>
2020-05-12 16:33:07 -0700

    Add logging for routing events. This helped me investigate routing related issues.

--- AngularApp/projects/app-service-diagnostics/src/app/app.component.ts
+++ AngularApp/projects/app-service-diagnostics/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, isDevMode } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, NavigationError, NavigationCancel } from '@angular/router';
 import { initializeIcons } from 'office-ui-fabric-react/lib/Icons';
 import { WebSitesService } from './resources/web-sites/services/web-sites.service';
 import { AuthService } from './startup/services/auth.service';
@@ -18,7 +18,17 @@ export class AppComponent implements OnInit {
         this._newVersionEnabled = value;
      }
 
+    private logRoutingEvents: boolean = false;
+
     constructor(private _router: Router) {
+        if(this.logRoutingEvents) {
+            this._router.events.forEach(event=>{
+                console.log(event)
+                if(event instanceof NavigationError || event instanceof NavigationCancel) {
+                    console.error('Routing navigation failure.');                    
+                }
+            });
+        }        
     }
 
     ngOnInit() {
