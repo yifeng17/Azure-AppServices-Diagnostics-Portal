50f5e4170594194b79c5df4b965252b228495780
Ajay Sharma <SharmaAjay19@users.noreply.github.com>
2020-06-08 11:35:03 -0700

    Enable Search in Applens for everyone (#772)
    * Enable Search in Applens for everyone
    
    * back navigation
    
    Co-authored-by: Ajay Sharma <ajsharm@microsoft.com>

--- AngularApp/projects/applens/src/app/modules/dashboard/dashboard/dashboard.component.ts
+++ AngularApp/projects/applens/src/app/modules/dashboard/dashboard/dashboard.component.ts
@@ -133,8 +133,6 @@ export class DashboardComponent implements OnDestroy {
   triggerSearch(){
     this._searchService.searchTerm = this._searchService.searchTerm.trim();
     if (this._searchService.searchIsEnabled && this._searchService.searchTerm && this._searchService.searchTerm.length>3){
-      this._searchService.searchId = uuid();
-      this._searchService.newSearch = true;
       this.navigateTo(`search`, {searchTerm: this._searchService.searchTerm}, 'merge');
     }
   }
