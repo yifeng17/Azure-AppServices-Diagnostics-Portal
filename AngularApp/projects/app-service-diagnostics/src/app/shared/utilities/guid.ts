39a88c4d5a212dafa5c91c05b476d868561c303a
nmallick1 <nmallick@microsoft.com>
2020-06-30 16:10:52 -0700

    Change this file due to strict compiler restrictions.

--- AngularApp/projects/app-service-diagnostics/src/app/shared/utilities/guid.ts
+++ AngularApp/projects/app-service-diagnostics/src/app/shared/utilities/guid.ts
@@ -1,33 +1,38 @@
 export class Guid {
+    private static readonly guidRegex = new RegExp('[xy]', 'g');
     static newGuid(): string {
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        let returnValue = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(Guid.guidRegex, function(c) {
             var r = (Math.random() * 16) | 0,
                 v = c === 'x' ? r : (r & 0x3) | 0x8;
             return v.toString(16);
         });
+        return returnValue;
     }
     static newShortGuid(): string {
-        return 'xxxxxxxx-yxxx'.replace(/[xy]/g, function(c) {
+        let returnValue = 'xxxxxxxx-yxxx'.replace(Guid.guidRegex, function(c) {
             var r = (Math.random() * 16) | 0,
                 v = c === 'x' ? r : (r & 0x3) | 0x8;
             return v.toString(16);
         });
+        return returnValue;
     }
     static newTinyGuid(): string {
-        return 'yxxx'.replace(/[xy]/g, function(c) {
+        let returnValue = 'yxxx'.replace(Guid.guidRegex, function(c) {
             var r = (Math.random() * 16) | 0,
                 v = c === 'x' ? r : (r & 0x3) | 0x8;
             return v.toString(16);
         });
+        return returnValue;
     }
 
     static newCustomGuid(length: number): string {
         if (length > 0) {
-            return 'x'.repeat(length).replace(/[xy]/g, c => {
+            let returnValue = 'x'.repeat(length).replace(Guid.guidRegex, c => {
                 var r = (Math.random() * 16) | 0,
                     v = c === 'x' ? r : (r & 0x3) | 0x8;
                 return v.toString(16);
             });
+            return returnValue;
         }
 
         return '';
