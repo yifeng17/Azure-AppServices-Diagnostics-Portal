49d9809269dda217717417033866b983414b5ad3
Puneet Gupta <puneetg@microsoft.com>
2020-08-06 21:48:19 +0530

    Updating Beta Subscriptions (#861)

--- AngularApp/projects/app-service-diagnostics/src/app/betaSubscriptions.ts
+++ AngularApp/projects/app-service-diagnostics/src/app/betaSubscriptions.ts
@@ -35,6 +35,7 @@ export class DemoSubscriptions {
         '688dfd98-3a87-47d6-abd8-55e5e5e87c78',//Jeff Hopper's Subscription, external
         '82acd5bb-4206-47d4-9c12-a65db028483d',//Christian Smith,
         '59673966-501b-4a6c-9ed5-12a166a5bde9', // Testing mwh geo
-        '750f6b19-1621-45ba-a641-0eb3babe2141' // Tulika Chaudharie's Subscription
+        '750f6b19-1621-45ba-a641-0eb3babe2141', // Tulika Chaudharie's Subscription
+        'fa125e53-a44d-494b-a386-824498910cd4' //  Venkata Manusha Gopisetty's subscription
     ];
 }
