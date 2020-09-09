1092e3f79f9ae80c183d8ed42568db5fc46c200f
Ajay Sharma <SharmaAjay19@users.noreply.github.com>
2020-09-09 12:52:45 -0700

    Remove accessibility autofocus from search component by default (#900)
    * Read search results on Narrator
    
    * Remove accessibility autofocus by default from search component
    
    Co-authored-by: Ajay Sharma <ajsharm@microsoft.com>

--- AngularApp/projects/diagnostic-data/src/lib/components/detector-search/detector-search.component.ts
+++ AngularApp/projects/diagnostic-data/src/lib/components/detector-search/detector-search.component.ts
@@ -80,6 +80,7 @@ export class DetectorSearchComponent extends DataRenderBaseComponent implements 
     isListening: boolean = true;
     componentStartTime: number;
     showCharAlert: boolean = false;
+    autoFocus: boolean = false;
     
     @Input()
     withinDiagnoseAndSolve: boolean = false;
@@ -208,18 +209,21 @@ export class DetectorSearchComponent extends DataRenderBaseComponent implements 
     };
 
     announceAlert() {
-        this.showCharAlert = true;
-        setTimeout(() => {this.charAlertRef.nativeElement.focus();this.charAlertRef.nativeElement.click();}, 500);
-        setTimeout(() => {
-            this.searchInputBox.nativeElement.focus();
-        }, 5000);
+        if (this.autoFocus) {
+            this.showCharAlert = true;
+            setTimeout(() => {this.charAlertRef.nativeElement.focus();this.charAlertRef.nativeElement.click();}, 500);
+            setTimeout(() => {
+                this.searchInputBox.nativeElement.focus();
+            }, 5000);
+        }
     }
     
     resetAlert() {
         this.showCharAlert = false;
     }
 
-    hitSearch(){
+    hitSearch(autofocus: boolean=false){
+        if (autofocus) {this.autoFocus = true;}
         this.resetAlert();
         if (this.searchTerm && this.searchTerm.length > 1){
             const queryParams: Params = { searchTerm: this.searchTerm };
@@ -267,7 +271,9 @@ export class DetectorSearchComponent extends DataRenderBaseComponent implements 
         }));
         this.showPreLoader = true;
         observableForkJoin([searchTask, detectorsTask, childrenTask]).subscribe(results => {
-            setTimeout(() => {this.searchResultsSection.nativeElement.click();this.searchResultsSection.nativeElement.focus();}, 2000);
+            if (this.autoFocus) {
+                setTimeout(() => {this.searchResultsSection.nativeElement.click();this.searchResultsSection.nativeElement.focus();}, 2000);
+            }
             this.showPreLoader = false;
             var searchResults: DetectorMetaData[] = results[0];
             this.logEvent(TelemetryEventNames.SearchQueryResults, { parentDetectorId: this.detector, searchId: this.searchId, query: this.searchTerm, results: JSON.stringify(searchResults.map((det: DetectorMetaData) => new Object({ id: det.id, score: det.score }))), ts: Math.floor((new Date()).getTime() / 1000).toString() });
