06333edf63224ea25616c0900c8057d20c4b79b5
XiaoxuHU <tjhuxiaoxu@gmail.com>
2020-09-30 13:08:30 -0700

    Fix IE regex exception  (#924)
    * fix ie exception
    
    * fix ie exception
    
    * comment out regex
    
    * use new RegExp to create regex
    
    * Midify regex to ready for both IE and Chrome
    
    * remove unsed code
    
    * remove unwant change

--- AngularApp/projects/diagnostic-data/src/lib/utilities/pii-utilities.ts
+++ AngularApp/projects/diagnostic-data/src/lib/utilities/pii-utilities.ts
@@ -1,41 +1,75 @@
 export class PIIUtilities {
     public static removePII(text: string) {
         let res = text;
+
+        res = this.maskPhone(res);
+        res = this.maskEmails(res);
+        res = this.maskIPV4Address(res);
+        res = this.maskPassword(res);
+        res = this.maskQueryString(res);
+        return res;
+    }
+
+    //Use \\ to replace \,due to js string feature
+    public static maskPhone(text: string) {
+        let res = text;
         try {
-            res = this.maskPhone(res);
-            res = this.maskEmails(res);
-            res = this.maskIPV4Address(res);
-            res = this.maskPassword(res);
-            res = this.maskQueryString(res);
+            let regexStr = "(\\+?\\d?\\d?\\d?)([\\d\\-)\(\\s]{10})";
+            const regex = new RegExp(regexStr,"g");
+            res = text.replace(regex, " ********** ");
+        } catch (e) {
+            
+        }
+        return res;
+    }
+
+    public static maskEmails(text: string) {
+        let res = text;
+        try {
+            const regexStr = "(?<=[\\w]{1})[\\w-\\._\\+%]*(?=@([\\w-_]+)[\\.]{0})";
+            const regex = new RegExp(regexStr,"g");
+            res = text.replace(regex, s => "*".repeat(s.length));
         } catch (e) {
 
         }
         return res;
     }
 
-    public static maskPhone(text: string) {
-        const regex = /(\+?\d?\d?\d?)([\d\-)\(\s]{10})/g;
-        return text.replace(regex, " ********** ");
-    }
-
-    public static maskEmails(text: string) {
-        const regex = /(?<=[\w]{1})[\w-\._\+%]*(?=@([\w-_]+)[\.]{0})/g;
-        return text.replace(regex, s => "*".repeat(s.length));
-    }
-
     public static maskIPV4Address(text: string) {
-        const regex = /(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]|[0-9])/g;
-        return text.replace(regex, s => s.split(".")[0] + '.' + s.split(".")[1] + '.' + s.split(".")[2] + '.XXX'
-        );
+        let res = text;
+        try {
+            const regexStr = "(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]|[0-9])";
+
+            const regex = new RegExp(regexStr,"g");
+            res = text.replace(regex, s => s.split(".")[0] + '.' + s.split(".")[1] + '.' + s.split(".")[2] + '.XXX'
+            );
+        } catch (e) {
+
+        }
+        return res;
     }
 
     public static maskPassword(text: string) {
-        const regex = /(?<=(\bpass\b)|(\bpwd\b)|(\bpassword\b)|(\buserpass\b))[^\w\r\n]+(.+)/gi;
-        return text.replace(regex, ":****");
+        let res = text;
+        try {
+            const regexStr = "(?<=(\\bpass\\b)|(\\bpwd\\b)|(\\bpassword\\b)|(\\buserpass\\b))[^\\w\\r\\n]+(.+)";
+            const regex = new RegExp(regexStr,"gi");
+            res = text.replace(regex, ":****");
+        } catch (e) {
+
+        }
+        return res;
     }
 
     public static maskQueryString(text: string) {
-        const regex = /(?<=https?:\/\/[\w\.-_%]+\?)([\w-\._&%]+=[\w-\._%]+)+/g;
-        return text.replace(regex,"****");
-    }   
+        let res = text;
+        try {
+            const regexStr = "(?<=https?:\\/\\/[\\w\\.-_%]+\\?)([\\w-\\._&%]+=[\\w-\\._%]+)+";
+            const regex = new RegExp(regexStr,"g");
+            res = text.replace(regex, "****");
+        } catch (e) {
+
+        }
+        return res;
+    }
 }
