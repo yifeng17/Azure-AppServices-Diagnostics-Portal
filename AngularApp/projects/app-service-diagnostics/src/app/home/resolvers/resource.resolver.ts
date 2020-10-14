b2e4d8d5a47fb87ec58ec508f215055af6a4b06d
XiaoxuHU <tjhuxiaoxu@gmail.com>
2020-10-14 14:26:51 -0700

    Fix exception for ‘Uncaught (in promise): Code:MissingSubscription (#942)
    * Add another way to get resourceUri
    
    * Add Error Message
    
    * Add Error Msg for empty uri

--- AngularApp/projects/app-service-diagnostics/src/app/home/resolvers/resource.resolver.ts
+++ AngularApp/projects/app-service-diagnostics/src/app/home/resolvers/resource.resolver.ts
@@ -1,4 +1,4 @@
-import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
+import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { ResourceService } from '../../shared-v2/services/resource.service';
@@ -10,11 +10,33 @@ export class ResourceResolver implements Resolve<Observable<{} | ArmResource>> {
     constructor(private _resourceService: ResourceService, private _detectorControlService: DetectorControlService) { }
     // Live Chat Service is included here so that we ensure an instance is created
 
-    resolve(activatedRouteSnapshot: ActivatedRouteSnapshot): Observable<{} | ArmResource> {
+    resolve(activatedRouteSnapshot: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<{} | ArmResource> {
         if (!this._detectorControlService.startTime) {
 
             this._detectorControlService.setDefault();
         }
-        return this._resourceService.registerResource(activatedRouteSnapshot.parent.url.filter(x => x.path !== 'new' && x.path !== 'categories').join('/'));
+
+        //Try get resourceUri from activatedRoute,if not then get from routerstate
+        let resourceUri = activatedRouteSnapshot.parent.url
+            .filter(x => x.path !== 'new' && x.path !== 'categories')
+            .map(x => x.path)
+            .join('/');
+        if (!this.checkResourceUri(resourceUri)) {
+            const url = state.url;
+            const startIndex = url.indexOf("subscriptions/") > -1 ? url.indexOf("subscriptions/") : 0;
+            const endIndex = url.indexOf("/categories") > -1 ? url.indexOf("/categories") : url.length;
+
+            resourceUri = url.substring(startIndex, endIndex);
+        }
+        if (!this.checkResourceUri(resourceUri)) {
+            const urlFromActivatedRoute = activatedRouteSnapshot.parent.url.join('/');
+            throw new Error(`Empty ResourceUri from resource.resolver;Activated Route: ${urlFromActivatedRoute}; State Url: ${state.url}`);
+        }
+
+        return this._resourceService.registerResource(resourceUri);
+    }
+
+    private checkResourceUri(resourceUri: string): boolean {
+        return resourceUri !== "" && resourceUri !== "/";
     }
 }
