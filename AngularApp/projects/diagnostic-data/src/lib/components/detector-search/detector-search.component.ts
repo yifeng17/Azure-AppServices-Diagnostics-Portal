3f21241a65174d9b46c457a2fde94c0a236622cd
Ajay Sharma <SharmaAjay19@users.noreply.github.com>
2020-10-23 13:54:22 -0700

    Enable detector search in applens (internally) for Logic apps and APIM (#951)

--- AngularApp/projects/diagnostic-data/src/lib/components/detector-search/detector-search.component.ts
+++ AngularApp/projects/diagnostic-data/src/lib/components/detector-search/detector-search.component.ts
@@ -42,7 +42,7 @@ export class DetectorSearchComponent extends DataRenderBaseComponent implements 
     @ViewChild ('searchInputBox', {static: false}) searchInputBox: ElementRef;
     @ViewChild ('searchResultsSection', {static: false}) searchResultsSection: ElementRef;
     detectorSearchEnabledPesIds: string[] = ["14748", "16072", "16170"];
-    detectorSearchEnabledPesIdsInternal: string[] = ["14748", "16072", "16170", "16450"];
+    detectorSearchEnabledPesIdsInternal: string[] = ["14748", "16072", "16170", "16450", "15791", "15551"];
     startTime: Moment;
     endTime: Moment;
     isPublic: boolean = false;
