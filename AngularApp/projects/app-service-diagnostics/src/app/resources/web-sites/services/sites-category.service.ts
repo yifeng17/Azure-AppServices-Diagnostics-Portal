53701eae89b28a917ed86ae7c219e43a59696d29
XiaoxuHU <xiaoxhu@microsoft.com>
2021-01-31 15:14:34 -0800

    Update category tile style (#1021)
    * Update with category tile style
    
    * add logic for dynamic showing keywords
    
    * fix flex in IE
    
    * Add keyword for all resources
    
    * Update keywords for other category

--- AngularApp/projects/app-service-diagnostics/src/app/resources/web-sites/services/sites-category.service.ts
+++ AngularApp/projects/app-service-diagnostics/src/app/resources/web-sites/services/sites-category.service.ts
@@ -24,7 +24,7 @@ export class SitesCategoryService extends CategoryService {
         name: 'Availability and Performance',
         overviewDetectorId: 'AvailabilityAndPerformanceWindows',
         description: 'Check your app’s health and discover app or platform issues.',
-        keywords: ['Downtime', '5xx', '4xx', 'CPU', 'Memory'],
+        keywords: ['Downtime', '5xx', '4xx', 'CPU', 'Memory','SNAT'],
         color: 'rgb(208, 175, 239)',
         createFlowForCategory: false,
         chatEnabled: false
@@ -42,7 +42,7 @@ export class SitesCategoryService extends CategoryService {
         name: 'Availability and Performance',
         overviewDetectorId: 'LinuxAvailabilityAndPerformance',
         description: 'Is your app experiencing downtime or slowness? Discover issues that may impact SLA, caused by your app itself or Azure.',
-        keywords: ['Downtime', '5xx Errors', '4xx Errors', 'CPU', 'Memory'],
+        keywords: ['Downtime', '5xx Errors', '4xx Errors', 'CPU', 'Memory','SNAT'],
         color: 'rgb(208, 175, 239)',
         createFlowForCategory: false,
         chatEnabled: false
@@ -59,7 +59,7 @@ export class SitesCategoryService extends CategoryService {
         name: 'Configuration and Management',
         overviewDetectorId: 'ConfigurationAndManagement',
         description: 'Find out if your app service features are misconfigured.',
-        keywords: ['Backups', 'Slots', 'Swaps', 'Scaling'],
+        keywords: ['Backups', 'Slots', 'Swaps', 'Scaling','IP Config'],
         color: 'rgb(249, 213, 180)',
         createFlowForCategory: true,
         chatEnabled: false
@@ -76,7 +76,7 @@ export class SitesCategoryService extends CategoryService {
         name: 'SSL and Domains',
         overviewDetectorId: 'SSLandDomains',
         description: 'Discover issues with certificates and custom domains.',
-        keywords: ['4xx','Permissions','Auth','Binding'],
+        keywords: ['4xx','Permissions','Auth','Binding','Cert Failures'],
         color: 'rgb(186, 211, 245)',
         createFlowForCategory: true,
         chatEnabled: true
@@ -94,7 +94,7 @@ export class SitesCategoryService extends CategoryService {
         name: 'Risk Assessments',
         overviewDetectorId: 'BestPractices',
         description: 'Analyze your app for optimal performance and configurations.',
-        keywords: ['Autoscale','AlwaysOn','Density','ARR'],
+        keywords: ['Autoscale','AlwaysOn','Density','ARR','Health Check'],
         color: 'rgb(208, 228, 176)',
         createFlowForCategory: true,
         chatEnabled: false
@@ -188,7 +188,7 @@ export class SitesCategoryService extends CategoryService {
               name: 'Navigator (Preview)',
               overviewDetectorId:'navigator',
               description: 'Track changes on your app and its dependencies.',
-              keywords: ['Change Analysis', 'SQL', 'Dependency'],
+              keywords: ['Change Analysis', 'SQL', 'Dependency','Storage'],
               color: 'rgb(255, 217, 119)',
               createFlowForCategory: false,
               chatEnabled: false,
@@ -215,7 +215,7 @@ export class SitesCategoryService extends CategoryService {
         name: 'Diagnostic Tools',
         overviewDetectorId:'DiagnosticTools',
         description: 'Run language-specific tools for deeper investigation.',
-        keywords: ['Profiler', 'Memory Dump', 'Auto-Heal'],
+        keywords: ['Profiler', 'Memory Dump', 'Auto-Heal','Event Logs'],
         color: 'rgb(170, 192, 208)',
         createFlowForCategory: false,
         overridePath: `resource${siteId}/diagnosticTools`
