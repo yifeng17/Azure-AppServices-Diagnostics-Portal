2f10f02698daf517feb51b52061e124729ce78b6
michimune <michimune@outlook.com>
2021-03-12 13:09:10 -0800

    Support Tab UI (#1063)
    Co-authored-by: Michimune Kohno <mikono@microsoft.com>

--- AngularApp/projects/diagnostic-data/src/lib/diagnostic-data.module.ts
+++ AngularApp/projects/diagnostic-data/src/lib/diagnostic-data.module.ts
@@ -83,7 +83,7 @@ import { AppDependenciesComponent } from './components/app-dependencies/app-depe
 import { HighchartsChartModule } from 'highcharts-angular';
 import { HighchartsGraphComponent } from './components/highcharts-graph/highcharts-graph.component';
 import { FabNavModule } from './components/fab-nav/fab-nav.module';
-import { FabPanelModule, FabIconModule, FabChoiceGroupModule, FabSearchBoxModule, FabDropdownModule, FabDetailsListModule, FabTextFieldModule, FabMessageBarModule, FabButtonModule, FabTooltipModule, FabSpinnerModule, FabCalloutModule, FabCheckboxModule } from '@angular-react/fabric';
+import { FabPanelModule, FabIconModule, FabChoiceGroupModule, FabSearchBoxModule, FabDropdownModule, FabDetailsListModule, FabTextFieldModule, FabMessageBarModule, FabButtonModule, FabTooltipModule, FabSpinnerModule, FabCalloutModule, FabCheckboxModule, FabPivotModule } from '@angular-react/fabric';
 import { SummaryCardsComponent } from './components/summary-cards/summary-cards.component';
 import { InsightsV4Component } from './components/insights-v4/insights-v4.component';
 import { CardSelectionV4Component } from './components/card-selection-v4/card-selection-v4.component';
@@ -106,6 +106,7 @@ import { KeystoneInsightComponent } from './components/keystone-insight/keystone
 import { SolutionViewContainerComponent } from './components/solution-view-container/solution-view-container.component';
 import { FabDataTableFilterComponent } from './components/fab-data-table-filter/fab-data-table-filter.component';
 import { NotificationRenderingComponent } from './components/notification-rendering/notification-rendering.component';
+import { FabTabComponent } from './components/fab-tab/fab-tab.component';
 
 @NgModule({
   imports: [
@@ -130,7 +131,8 @@ import { NotificationRenderingComponent } from './components/notification-render
     FabTooltipModule,
     FabSpinnerModule,
     FabCalloutModule,
-    FabCheckboxModule
+    FabCheckboxModule,
+    FabPivotModule,
   ],
   providers: [
     ClipboardService
@@ -169,7 +171,8 @@ import { NotificationRenderingComponent } from './components/notification-render
     KeystoneInsightComponent,
     SolutionViewContainerComponent,
     FabDataTableFilterComponent,
-    NotificationRenderingComponent
+    NotificationRenderingComponent,
+    FabTabComponent,
   ],
   entryComponents: [DetectorListAnalysisComponent],
   exports: [
@@ -188,7 +191,8 @@ import { NotificationRenderingComponent } from './components/notification-render
     AppInsightsEnablementComponent,
     ConnectAppInsightsComponent,
     WebSearchComponent,
-    SolutionViewContainerComponent
+    SolutionViewContainerComponent,
+    FabTabComponent,
   ],
 })
 export class DiagnosticDataModule {
