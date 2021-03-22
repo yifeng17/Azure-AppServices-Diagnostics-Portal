da60de9013c66b2e34ddc9b12c9d22a8b88cc743
Cindy Peng <38216903+cindylovescoding@users.noreply.github.com>
2021-03-22 15:29:54 -0700

    Fix side nav item navigation (#1082)

--- AngularApp/projects/app-service-diagnostics/src/app/shared-v2/services/feature.service.ts
+++ AngularApp/projects/app-service-diagnostics/src/app/shared-v2/services/feature.service.ts
@@ -162,10 +162,10 @@ export class FeatureService {
   }
 
   private getCategoryIdByCategoryName(name: string): string {
-    //Default set to "*",so it will still route to category-summary 
+    //Default set to "*",so it will still route to category-summary
     let categoryId: string = this.categories.length > 0 ? this.categories[0].id : "*";
-    const currentCategoryId = this._activatedRoute.root.firstChild.firstChild.firstChild.firstChild.snapshot.params["category"];
-    //If category name is "XXX Tools" and has Diagnostic Tools category,then should belong to Diagnostic Tool Category.For now this should be working in Windows Web App 
+    const currentCategoryId = this._activatedRoute.root.firstChild.firstChild.firstChild.firstChild.firstChild.snapshot.params["category"];
+    //If category name is "XXX Tools" and has Diagnostic Tools category,then should belong to Diagnostic Tool Category.For now this should be working in Windows Web App
     if ((name === "Diagnostic Tools" || name === "Support Tools" || name === "Proactive Tools") && this.categories.find(category => category.name === "Diagnostic Tools")) {
       const category = this.categories.find(category => category.name === "Diagnostic Tools");
       categoryId = category.id;
