1b74f55d0b616347bd4af42ce091abf1c5fae502
XiaoxuHU <xiaoxhu@microsoft.com>
2021-04-13 14:00:08 -0700

    Add for hide time-picker

--- AngularApp/projects/app-service-diagnostics/src/app/home/home.module.ts
+++ AngularApp/projects/app-service-diagnostics/src/app/home/home.module.ts
@@ -165,8 +165,7 @@ export const HomeRoutes = RouterModule.forChild([
                                 path: 'drilldownDetector/:drilldownDetectorName',
                                 component: GenericDetectorComponent,
                                 data: {
-                                    cacheComponent: false,
-                                    analysisMode: true
+                                    cacheComponent: false
                                 }
                             }
                         ],
