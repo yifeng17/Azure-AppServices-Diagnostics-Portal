0d3fef203c2dab5a9ad848656a9a5dc116803f31
Cindy Peng <xipeng@microsoft.com>
2021-05-13 12:17:41 -0700

    Enable single tenant logic app and fix applens apptype

--- AngularApp/projects/app-service-diagnostics/src/app/resources/web-sites/services/sites-category.service.ts
+++ AngularApp/projects/app-service-diagnostics/src/app/resources/web-sites/services/sites-category.service.ts
@@ -100,7 +100,75 @@ export class SitesCategoryService extends CategoryService {
         chatEnabled: false
       }
     },
-
+    // Workflow App
+    {
+        appType: AppType.WorkflowApp,
+        platform: OperatingSystem.windows | OperatingSystem.linux,
+        stack: '',
+        sku: Sku.All,
+        hostingEnvironmentKind: HostingEnvironmentKind.All,
+        item: {
+          id: 'AvailabilityAndPerformanceLogicApp',
+          overviewDetectorId: 'AvailabilityAndPerformanceLogicApp',
+          name: 'Availability and Performance',
+          description: 'Investigate performance issues or just check the health of your Logic App.',
+          keywords: ['Downtime', '5xx Errors', '4xx Errors', 'CPU', 'Memory', 'Slowness'],
+          color: 'rgb(208, 175, 239)',
+          createFlowForCategory: true,
+          chatEnabled: false
+        }
+      },
+      {
+          appType: AppType.WorkflowApp,
+          platform: OperatingSystem.windows | OperatingSystem.linux,
+          stack: '',
+          sku: Sku.All,
+          hostingEnvironmentKind: HostingEnvironmentKind.All,
+          item: {
+            id: 'ConfigurationAndManagementLogicApp',
+            overviewDetectorId: 'logicsettings',
+            name: 'Configuration and Management',
+            description: 'Find out if you misconfigured Logic App features/settings.',
+            keywords: ['Scaling', 'Swaps', 'Failed Backups', 'IPs', 'Migration'],
+            color: 'rgb(249, 213, 180)',
+            createFlowForCategory: true,
+            chatEnabled: false
+          }
+        },
+        {
+          appType: AppType.WorkflowApp,
+          platform: OperatingSystem.windows,
+          stack: '',
+          sku: Sku.All,
+          hostingEnvironmentKind: HostingEnvironmentKind.All,
+          item: {
+            id: 'SSLandDomainsLogicApp',
+            name: 'SSL and Domains',
+            overviewDetectorId:'SSLandDomainsLogicApp',
+            description: 'Investigate issues with certificates, authentication, and custom domains.',
+            keywords: ['4xx Errors', 'SSL', 'Domains', 'Permissions', 'Auth', 'Cert'],
+            color: 'rgb(186, 211, 245)',
+            createFlowForCategory: true,
+            chatEnabled: true
+          }
+        },
+        {
+          appType: AppType.WorkflowApp,
+          platform: OperatingSystem.windows,
+          stack: '',
+          sku: Sku.All,
+          hostingEnvironmentKind: HostingEnvironmentKind.All,
+          item: {
+            id: 'RiskAssessmentsLogicApp',
+            name: 'Risk Assessments',
+            overviewDetectorId: 'BestPracticesLogicApp',
+            description: 'Analyze your app for optimal performance and configuration.',
+            keywords: ['BestPractices', 'AlwaysOn', 'Async Pattern', 'Deployment Settings'],
+            color: 'rgb(208, 228, 176)',
+            createFlowForCategory: true,
+            chatEnabled: false
+          }
+        },
     // Function App
     {
       appType: AppType.FunctionApp,
