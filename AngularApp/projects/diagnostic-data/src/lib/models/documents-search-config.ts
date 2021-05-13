5a8f2262d39d0062ebae43887c9258ad705c9a00
Vijay Saayi <Vijay.Saayi@microsoft.com>
2021-05-14 03:03:20 +0530

    Enabling Deep Search for three other support topics (#1153)
    * Removed document-search component from detector-list-analysis as Deep Search is not enabled for Dynamic Analysis
    
    * Fixed bug which shows all Deep Search results as being surfaced by Bing
    
    * Enabling deep search for 3 other support topics

--- AngularApp/projects/diagnostic-data/src/lib/models/documents-search-config.ts
+++ AngularApp/projects/diagnostic-data/src/lib/models/documents-search-config.ts
@@ -1,7 +1,7 @@
 export class DocumentSearchConfiguration{
     documentSearchEnabledSupportTopicIds = {
-        "14748" : ["32542209"],
-        "16072" : ["32542209"]
+        "14748" : ["32542209", "32542211", "32444082", "32440122"],
+        "16170" : ["32542209", "32542211", "32444082", "32440122"]
     };
 
     documentSearchEnabledPesIdsInternal: string[] = ["14748", "16072", "16170"];
