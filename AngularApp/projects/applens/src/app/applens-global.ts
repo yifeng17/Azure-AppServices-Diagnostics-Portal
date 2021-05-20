227ec8873fc1f9db221b10ecffc63a7364f8d419
XiaoxuHU <xiaoxhu@microsoft.com>
2021-05-20 12:53:42 -0700

    Update style in landing page

--- AngularApp/projects/applens/src/app/applens-global.ts
+++ AngularApp/projects/applens/src/app/applens-global.ts
@@ -1,11 +1,12 @@
 import { Injectable } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { AdalService } from "adal-angular4";
 import { BehaviorSubject } from "rxjs";
 import { L2SideNavType } from "./modules/dashboard/l2-side-nav/l2-side-nav.component";
 
 @Injectable()
 export class ApplensGlobal {
-    constructor(private _route:ActivatedRoute) {}
+    constructor(private _route:ActivatedRoute,private _adalService:AdalService) {}
     openL2SideNavSubject:BehaviorSubject<L2SideNavType> = new BehaviorSubject<L2SideNavType>(L2SideNavType.None);
 
     dashboardTitleSubject:BehaviorSubject<string> = new BehaviorSubject<string>("");
@@ -14,5 +15,10 @@ export class ApplensGlobal {
     getDetectorName():string {
         const detectorId = this._route.firstChild.firstChild.firstChild.firstChild.firstChild.snapshot.params["detector"];
         return detectorId;
+    }
+
+    getUserAlias():string {
+        const alias:string = this._adalService.userInfo.profile ? this._adalService.userInfo.profile.upn : '';
+        return alias;
     }
 }
