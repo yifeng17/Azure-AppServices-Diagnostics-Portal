2517778bf92fa4d953d6bcea678b5bfbe4c4ee5a
XiaoxuHU <xiaoxhu@microsoft.com>
2021-05-20 14:10:28 -0700

    Update styles

--- AngularApp/projects/applens/src/app/modules/dashboard/tabs/tab-analysis/tab-analysis.component.ts
+++ AngularApp/projects/applens/src/app/modules/dashboard/tabs/tab-analysis/tab-analysis.component.ts
@@ -4,6 +4,7 @@ import { ApplensDiagnosticService } from '../../services/applens-diagnostic.serv
 import { DetectorListAnalysisComponent } from 'diagnostic-data';
 import { DownTime, zoomBehaviors } from 'diagnostic-data';
 import { ApplensCommandBarService } from '../../services/applens-command-bar.service';
+import { ApplensGlobal } from 'projects/applens/src/app/applens-global';
 
 @Component({
   selector: 'tab-analysis',
@@ -21,7 +22,7 @@ export class TabAnalysisComponent implements OnInit {
   @ViewChild('detectorListAnalysis', { static: true }) detectorListAnalysis: DetectorListAnalysisComponent
   downtimeZoomBehavior = zoomBehaviors.Zoom;
 
-  constructor(private _activatedRoute: ActivatedRoute, private _router: Router, private _diagnosticService: ApplensDiagnosticService,private _applensCommandBarService:ApplensCommandBarService) {
+  constructor(private _activatedRoute: ActivatedRoute, private _router: Router, private _diagnosticService: ApplensDiagnosticService,private _applensCommandBarService:ApplensCommandBarService,private _applensGlobal:ApplensGlobal) {
     this._activatedRoute.paramMap.subscribe(params => {
       this.analysisId = params.get('analysisId');
     });
@@ -58,5 +59,9 @@ export class TabAnalysisComponent implements OnInit {
     this._applensCommandBarService.getDetectorMeatData(this.analysisId).subscribe(metaData =>{
       this._applensCommandBarService.emailToAuthor(metaData);
     });
+  }
+
+  openFeedback() {
+    this._applensGlobal.openFeedback = true;
   }
 }
