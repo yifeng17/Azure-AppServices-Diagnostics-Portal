b757107790e41af3689c93f279dd7987ec675200
XiaoxuHU <xiaoxhu@microsoft.com>
2021-05-19 17:16:49 -0700

    Enable feebback in Applens

--- AngularApp/projects/applens/src/app/modules/dashboard/tabs/tab-common/tab-common.component.ts
+++ AngularApp/projects/applens/src/app/modules/dashboard/tabs/tab-common/tab-common.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { ApplensGlobal } from 'projects/applens/src/app/applens-global';
-import { flatMap, map } from 'rxjs/operators';
-import { ApplensDiagnosticService } from '../../services/applens-diagnostic.service';
 
 @Component({
   selector: 'tab-common',
@@ -11,18 +7,10 @@ import { ApplensDiagnosticService } from '../../services/applens-diagnostic.serv
 })
 export class TabCommonComponent implements OnInit {
 
-  contentHeight: string;
-  constructor(private _activatedRoute:ActivatedRoute,private _applensGlobal:ApplensGlobal,private _applensDiagnosticService:ApplensDiagnosticService) {
-    // this.contentHeight = (window.innerHeight - 112) + 'px';
+  constructor() {
+    
   }
 
   ngOnInit() {
-    const observable = this._activatedRoute.params.pipe(flatMap(params => {
-     return this._applensDiagnosticService.getDetectorMetaDataById(params["detector"]);
-    }));
-
-    observable.subscribe(metaData => {
-      this._applensGlobal.dashboardTitleSubject.next(metaData.name);
-    })
   }
 }
