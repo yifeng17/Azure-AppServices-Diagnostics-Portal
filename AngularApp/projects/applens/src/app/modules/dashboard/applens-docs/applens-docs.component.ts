6578ff63ae2ed381c271dc453551b6ef4e245251
XiaoxuHU <xiaoxhu@microsoft.com>
2021-06-22 14:20:21 -0700

    Update L1-side-nav and add docs

---
+++ AngularApp/projects/applens/src/app/modules/dashboard/applens-docs/applens-docs.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'applens-docs',
+  templateUrl: './applens-docs.component.html',
+  styleUrls: ['./applens-docs.component.scss']
+})
+export class ApplensDocsComponent {
+
+  constructor() { }
+}
