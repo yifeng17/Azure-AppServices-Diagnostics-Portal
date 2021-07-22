7b989ad89b468023b952b9528d5868a754cc9be5
XiaoxuHU <xiaoxhu@microsoft.com>
2021-07-22 12:15:20 -0700

    Move App name to header

--- AngularApp/projects/applens/src/app/modules/dashboard/user-detectors/user-detectors.component.ts
+++ AngularApp/projects/applens/src/app/modules/dashboard/user-detectors/user-detectors.component.ts
@@ -4,7 +4,6 @@ import { Router, ActivatedRoute, NavigationExtras } from '@angular/router';
 import { ApplensDiagnosticService } from '../services/applens-diagnostic.service';
 import { DataTableResponseColumn, DataTableResponseObject, DetectorMetaData, SupportTopic, TableColumnOption, TableFilterSelectionOption } from 'diagnostic-data';
 import { ApplensSupportTopicService } from '../services/applens-support-topic.service';
-import { DetectorItem } from '../category-page/category-page.component';
 
 @Component({
   selector: 'user-detectors',
@@ -14,15 +13,10 @@ import { DetectorItem } from '../category-page/category-page.component';
 export class UserDetectorsComponent implements OnInit {
 
   userId: string = "";
-  userInfo: UserInfo = undefined;
-  detectorsOfAuthor: DetectorMetaData[] = undefined;
-  detectors: DetectorItem[] = [];
+  isDetector: boolean = true;
   detectorsNumber: number = 0;
   isCurrentUser: boolean = false;
-  table: DataTableResponseObject = {
-    columns: [],
-    rows: [[]]
-  };
+  table: DataTableResponseObject = null;
   supportTopics: any[] = [];
   columnOptions: TableColumnOption[] = [
     {
@@ -34,34 +28,31 @@ export class UserDetectorsComponent implements OnInit {
   constructor(private _router: Router, private _activatedRoute: ActivatedRoute, private _diagnosticService: ApplensDiagnosticService, private _adalService: AdalService, private _supportTopicService: ApplensSupportTopicService) { }
 
   ngOnInit() {
-    this.detectorsOfAuthor = undefined;
-    this.userId = this._activatedRoute.snapshot.params['userId'];
+    this.isDetector = this._activatedRoute.snapshot.data["isDetector"];
     this.checkIsCurrentUser();
-    this._activatedRoute.params.subscribe(params => {
-      this.checkIsCurrentUser();
-      this.detectors = [];
 
+    if (this.isDetector) {
       this._supportTopicService.getSupportTopics().subscribe(supportTopics => {
         this.supportTopics = supportTopics;
         this._diagnosticService.getDetectors().subscribe((detectors: DetectorMetaData[]) => {
-          // Get all the detectors of the current author
-          this.detectorsOfAuthor = detectors.filter(detector => detector.author && detector.author.toLowerCase().indexOf(this.userId.toLowerCase()) > -1);          
-          this.table = this.generateDataTableObject(this.detectorsOfAuthor);
+          const detectorsOfAuthor = detectors.filter(detector => detector.author && detector.author.toLowerCase().indexOf(this.userId.toLowerCase()) > -1);
+          this.table = this.generateDetectorTable(detectorsOfAuthor);
         });
       });
+    } else {
+      this._diagnosticService.getGists().subscribe(gists => {
+        const gistsOfAuthor = gists.filter(gist => gist.author && gist.author.toLowerCase().indexOf(this.userId.toLowerCase()) > -1);
+        this.table = this.generateGistsTable(gistsOfAuthor);
+      });
+    }
+
+
+    this._activatedRoute.params.subscribe(params => {
+      this.checkIsCurrentUser();
     });
   }
 
-  navigateTo(path: string) {
-    let navigationExtras: NavigationExtras = {
-      queryParamsHandling: 'preserve',
-      preserveFragment: true,
-      relativeTo: this._activatedRoute
-    };
-    this._router.navigate([path], navigationExtras);
-  }
-
-  private generateDataTableObject(detectors: DetectorMetaData[]) {
+  private generateDetectorTable(detectors: DetectorMetaData[]) {
     const columns: DataTableResponseColumn[] = [
       { columnName: "Name" },
       { columnName: "Category" },
@@ -83,12 +74,40 @@ export class UserDetectorsComponent implements OnInit {
       const supportTopics = this.getSupportTopicName(detector.supportTopicList);
       return [name, category, supportTopics];
     });
-
     const dataTableObject: DataTableResponseObject = {
       columns: columns,
       rows: rows
     }
 
+    return dataTableObject;
+  }
+
+  private generateGistsTable(gists: DetectorMetaData[]) {
+
+    const columns: DataTableResponseColumn[] = [
+      { columnName: "Name" },
+      { columnName: "Category" }
+    ];
+
+    let rows: any[][] = [];
+
+    rows = gists.map(gist => {
+      let path = `../../../gists/${gist.id}`;
+      if (this.isCurrentUser) {
+        path = path + "/edit";
+      }
+      const name =
+        `<markdown>
+          <a href="${path}">${gist.name}</a>
+        </markdown>`;
+      const category = gist.category ? gist.category : "None";
+      const supportTopics = this.getSupportTopicName(gist.supportTopicList);
+      return [name, category, supportTopics];
+    });
+    const dataTableObject: DataTableResponseObject = {
+      columns: columns,
+      rows: rows
+    }
     return dataTableObject;
   }
 
@@ -99,17 +118,17 @@ export class UserDetectorsComponent implements OnInit {
     this.isCurrentUser = currentUser.toLowerCase() === this.userId;
   }
 
-  private getSupportTopicName(supportTopicIds:SupportTopic[]):string {
+  private getSupportTopicName(supportTopicIds: SupportTopic[]): string {
     const l2NameSet = new Set<string>();
     supportTopicIds.forEach(t => {
       const topic = this.supportTopics.find(topic => topic.supportTopicId === t.id);
-      if(topic && topic.supportTopicL2Name){
+      if (topic && topic.supportTopicL2Name) {
         l2NameSet.add(topic.supportTopicL2Name);
       }
     });
     const supportTopicNames = new Array(l2NameSet);
 
-    if(l2NameSet.size === 0) return "None";
+    if (l2NameSet.size === 0) return "None";
     return supportTopicNames.join(";");
   }
 }
