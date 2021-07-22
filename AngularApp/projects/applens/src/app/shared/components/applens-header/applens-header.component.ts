f0200c40cd8f5066b96a6e870335f7d33fb0e04e
XiaoxuHU <xiaoxhu@microsoft.com>
2021-07-22 12:15:20 -0700

    Move App name to header

--- AngularApp/projects/applens/src/app/shared/components/applens-header/applens-header.component.ts
+++ AngularApp/projects/applens/src/app/shared/components/applens-header/applens-header.component.ts
@@ -1,9 +1,7 @@
 import { Component, Injector, OnInit, Optional } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AdalService } from 'adal-angular4';
-import { DiagnosticService } from 'diagnostic-data';
-import { ApplensGlobal } from '../../../applens-global';
-import { ApplensDiagnosticService } from '../../../modules/dashboard/services/applens-diagnostic.service';
+import { ResourceInfo } from '../../models/resources';
 import { DiagnosticApiService } from '../../services/diagnostic-api.service';
 
 @Component({
@@ -14,19 +12,17 @@ import { DiagnosticApiService } from '../../services/diagnostic-api.service';
 export class ApplensHeaderComponent implements OnInit {
   userPhotoSource: string = "";
   applensLogo: string = "../../../../assets/img/Applens-Logo.svg";
-  constructor(private _adalService:AdalService,private _diagnosticApiService:DiagnosticApiService, private _injector: Injector,private _activatedRoute:ActivatedRoute) { }
+  resourceInfo: ResourceInfo = new ResourceInfo();
+  constructor(private _adalService: AdalService, private _diagnosticApiService: DiagnosticApiService,private _activatedRoute:ActivatedRoute) { }
 
   ngOnInit() {
     const alias = this._adalService.userInfo.profile ? this._adalService.userInfo.profile.upn : '';
     const userId = alias.replace('@microsoft.com', '');
-      this._diagnosticApiService.getUserPhoto(userId).subscribe(image => {
-        this.userPhotoSource = image;
+    this._diagnosticApiService.getUserPhoto(userId).subscribe(image => {
+      this.userPhotoSource = image;
     });
-  }
-
-  getAppName():string{
-    const parms = this._activatedRoute.snapshot.params;
-    // const _applensDiagnosticService = this._injector.get(ApplensGlobal);
-    return "App Name";
+    if(this._activatedRoute.snapshot.data["info"]) {
+      this.resourceInfo = this._activatedRoute.snapshot.data["info"];
+    }
   }
 }
