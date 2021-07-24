39c23ccd26244eb662412b593de77a89e8ed33d1
XiaoxuHU <xiaoxhu@microsoft.com>
2021-07-23 17:46:26 -0700

    enable ASE for V3 (#1224)

--- AngularApp/projects/app-service-diagnostics/src/app/fabric-ui/version-test.service.ts
+++ AngularApp/projects/app-service-diagnostics/src/app/fabric-ui/version-test.service.ts
@@ -12,7 +12,8 @@ export const allowV3PResourceTypeList: { type: string, allowSwitchBack: boolean 
     { type: "microsoft.apimanagement/service", allowSwitchBack: false },
     { type: "microsoft.signalrservice/signalr", allowSwitchBack: false },
     { type: "microsoft.logic/workflows", allowSwitchBack: false},
-    { type: "microsoft.logic/integrationserviceenvironments", allowSwitchBack: false}
+    { type: "microsoft.logic/integrationserviceenvironments", allowSwitchBack: false},
+    { type: "microsoft.web/hostingenvironments", allowSwitchBack: false }
 ];
 
 
