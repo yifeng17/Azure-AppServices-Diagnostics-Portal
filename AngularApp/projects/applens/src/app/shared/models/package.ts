410363f10ecb5d31c2efcf76b5127f344b963cef
XiaoxuHU <xiaoxhu@microsoft.com>
2021-10-01 11:23:43 -0700

    changes for onboardingflow (#1264)
    Co-authored-by: Darrel Donald <darreldonald@microsoft.com>

--- AngularApp/projects/applens/src/app/shared/models/package.ts
+++ AngularApp/projects/applens/src/app/shared/models/package.ts
@@ -1,16 +1,16 @@
 
 export interface Package {
-    codeString: string;
-    id: string;
-    committedByAlias: string;
-    packageConfig: string;
-    dllBytes: string;
+  codeString: string;
+  id: string;
+  committedByAlias: string;
+  packageConfig: string;
+  dllBytes: string;
   pdbBytes: string;
   metadata: string;
 }
 
 export interface Dependency {
-    name: string;
-    version: string;
-    allVersions: string[];
+  name: string;
+  version: string;
+  allVersions: string[];
 }
