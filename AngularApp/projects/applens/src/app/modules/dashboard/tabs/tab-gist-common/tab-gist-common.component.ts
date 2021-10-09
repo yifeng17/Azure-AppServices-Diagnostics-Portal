8b59ea9e15a4b5c42438899cd525e994888f14a8
Darrel Donald <darreldonald@microsoft.com>
2021-10-08 18:43:54 -0700

    changes discussed in last meeting

--- AngularApp/projects/applens/src/app/modules/dashboard/tabs/tab-gist-common/tab-gist-common.component.ts
+++ AngularApp/projects/applens/src/app/modules/dashboard/tabs/tab-gist-common/tab-gist-common.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { TabKey, Tab } from '../tab-key';
 
 @Component({
   selector: 'tab-gist-common',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TabGistCommonComponent implements OnInit {
   contentHeight: string;
-  constructor() { }
+  tabs: Tab[] = [
+    {
+      headerText: "Develop",
+      itemKey: TabKey.Develop
+    },
+    {
+      headerText: "Commit History",
+      itemKey: TabKey.CommitHistory
+    }
+  ];
+  selectedTabKey: string;
+  constructor(private _router: Router, private _activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this._router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe(e => {
+      const key: string = this._activatedRoute.firstChild.snapshot.data["tabKey"];
+      this.selectedTabKey = key ? key : this.tabs[0].itemKey;
+    });
     this.contentHeight = (window.innerHeight - 112) + 'px';
+  }
+
+  navigateToData(ev: any) {
+    const key: string = ev.item.props.itemKey;
+
+    switch (key) {
+      case TabKey.Develop:
+        this._router.navigate(["./"], {
+          relativeTo: this._activatedRoute
+        });
+        break;
+      case TabKey.CommitHistory:
+        this._router.navigate(["changelist"], {
+          relativeTo: this._activatedRoute
+        });
+        break;
+    }
   }
 }
