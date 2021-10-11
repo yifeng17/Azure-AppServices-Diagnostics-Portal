332c52202b94a2e076310d609ef3ef7f20621cb9
XiaoxuHU <xiaoxhu@microsoft.com>
2021-10-11 09:27:52 -0700

    Update the changes for Applens

--- AngularApp/projects/applens/src/app/shared/utilities/applens-docs-constant.ts
+++ AngularApp/projects/applens/src/app/shared/utilities/applens-docs-constant.ts
@@ -1,28 +1,32 @@
 export const applensDocs: { name: string, link: string } [] = [
     {
         name: "AppLens Documentation",
-        link: "https://applensdiagnostics.azurewebsites.net/"
+        link: "https://applensdiagnostics.azurewebsites.net/?p=262"
     },
     {
-        name: "On-board a new resource type",
+        name: "Onboard your service to AppLens",
         link: "https://applensdiagnostics.azurewebsites.net/?p=76"
     },
     {
-        name: "Detector Best Practices",
-        link: "https://applensdiagnostics.azurewebsites.net/?p=59"
+        name: "Enable the Diagnose & Solve problems blade",
+        link: "https://applensdiagnostics.azurewebsites.net/?p=79"
     },
     {
-        name:"Project Shield – NLP based Diagnostic",
-        link:"https://applensdiagnostics.azurewebsites.net/?p=262"
+        name:"Create Your Own Hello World Detector",
+        link:"https://msit.microsoftstream.com/video/b2d50840-98dc-ae70-d6d3-f1ec17fb5da9"
 
     },
     {
-        name:"Keystone – High confidence solutions",
-        link:"https://applensdiagnostics.azurewebsites.net/?p=574"
+        name:"Make Your Detector Customer-Ready",
+        link:"https://microsoft-my.sharepoint.com/personal/yunjchoi_microsoft_com/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fyunjchoi%5Fmicrosoft%5Fcom%2FDocuments%2FRecordings%2FAppLens%20101%20Workshop%20%2D%20Continued%2D20210922%5F090555%2DMeeting%20Recording%2Emp4&parent=%2Fpersonal%2Fyunjchoi%5Fmicrosoft%5Fcom%2FDocuments%2FRecordings&originalPath=aHR0cHM6Ly9taWNyb3NvZnQtbXkuc2hhcmVwb2ludC5jb20vOnY6L3AveXVuamNob2kvRWZacXVjUVJwOUJQbDl0VlY2Y1g3ZFFCbW13MlRnWFQ1LUJTUlZ6eTdhNE5HQT9ydGltZT12eUdRanRLTTJVZw"
 
     },
     {
-        name:"Training Videos",
-        link:"https://applensdiagnostics.azurewebsites.net/?p=403"
+        name:"Make your Detector Production-Ready",
+        link:"https://microsoft-my.sharepoint.com/personal/nmallick_microsoft_com/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fnmallick%5Fmicrosoft%5Fcom%2FDocuments%2FRecordings%2FAppLens%20101%20Workshop%20%2D%20Continued%2D20210922%5F095038%2DMeeting%20Recording%2Emp4&parent=%2Fpersonal%2Fnmallick%5Fmicrosoft%5Fcom%2FDocuments%2FRecordings&originalPath=aHR0cHM6Ly9taWNyb3NvZnQtbXkuc2hhcmVwb2ludC5jb20vOnY6L3Avbm1hbGxpY2svRVZDZkVqbDV5bGhGbXo4ME9ybnRERHNCbFVtSkNKR0Zib29RTnZ0dkVQSGNnQT9ydGltZT1qUXAxbDlLTTJVZw"
+    },
+    {
+        name: "Join AppLens Partners to stay up to date",
+        link: "https://idweb.microsoft.com/IdentityManagement/aspx/groups/AllDLs.aspx?popupFromClipboard=%2Fidentitymanagement%2Faspx%2FGroups%2FEditGroup.aspx%3Fid%3D52b985a8-a437-4106-b8ad-8e9d6c7ac5a4"
     }
 ];
