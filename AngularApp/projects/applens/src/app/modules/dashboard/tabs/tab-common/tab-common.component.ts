23b0e23872e43654a43ef0eb5acb13f586c0c2d3
XiaoxuHU <xiaoxhu@microsoft.com>
2021-10-15 15:24:04 -0700

    Update with enable/disable develop by appsettings

--- AngularApp/projects/applens/src/app/modules/dashboard/tabs/tab-common/tab-common.component.ts
+++ AngularApp/projects/applens/src/app/modules/dashboard/tabs/tab-common/tab-common.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { DiagnosticApiService } from 'projects/applens/src/app/shared/services/diagnostic-api.service';
 import { distinct } from 'rxjs-compat/operator/distinct';
 import { filter } from 'rxjs/operators';
 import { Tab, TabKey } from '../tab-key';
@@ -12,13 +13,16 @@ import { Tab, TabKey } from '../tab-key';
 export class TabCommonComponent implements OnInit {
 
   tabs: Tab[] = [
-    { headerText: "Data", itemKey: TabKey.Data },
-    { headerText: "Develop", itemKey: TabKey.Develop }
+    { headerText: "Data", itemKey: TabKey.Data }
   ];
+
+  developTab: Tab = {
+    headerText: "Develop", itemKey: TabKey.Develop 
+  }
 
   selectedTabKey:string;
 
-  constructor(private _router: Router, private _activatedRoute: ActivatedRoute) {
+  constructor(private _router: Router, private _activatedRoute: ActivatedRoute,private _diagnosticApiService:DiagnosticApiService) {
     this._activatedRoute.firstChild.data.subscribe(data => {
       const key:string = data["tabKey"];
       this.selectedTabKey = key ? key : this.tabs[0].itemKey;
@@ -26,6 +30,11 @@ export class TabCommonComponent implements OnInit {
   }
 
   ngOnInit() {
+    this._diagnosticApiService.getEnableDetectorDevelopment().subscribe(enabledDetectorDevelopment => {
+      if(enabledDetectorDevelopment) {
+        this.tabs.push(this.developTab);
+      }
+    });
     this._router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe(e => {
       const key:string = this._activatedRoute.firstChild.snapshot.data["tabKey"];
       this.selectedTabKey = key ? key : this.tabs[0].itemKey;
