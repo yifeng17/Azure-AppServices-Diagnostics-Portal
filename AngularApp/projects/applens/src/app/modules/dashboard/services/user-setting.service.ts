12cac6eb3249f64f23249c5561f702f4aac9a21d
XiaoxuHU <xiaoxhu@microsoft.com>
2021-10-20 11:36:39 -0700

    Fix updating recent resource

--- AngularApp/projects/applens/src/app/modules/dashboard/services/user-setting.service.ts
+++ AngularApp/projects/applens/src/app/modules/dashboard/services/user-setting.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { AdalService } from "adal-angular4";
-import { map, flatMap, catchError, tap } from "rxjs/operators";
+import { map, catchError } from "rxjs/operators";
 import { of } from "rxjs";
 import { RecentResource, UserSetting, } from "../../../shared/models/user-setting";
 import { DiagnosticApiService } from "../../../shared/services/diagnostic-api.service";
@@ -13,59 +13,54 @@ export class UserSettingService {
     constructor(private _diagnosticApiService: DiagnosticApiService, private _adalService: AdalService) {
         const alias = this._adalService.userInfo.profile ? this._adalService.userInfo.profile.upn : '';
         this.userId = alias.replace('@microsoft.com', '');
-        this._diagnosticApiService.getUserSetting(this.userId).subscribe(userInfo => {
-            this.userSetting = userInfo;
-        }, err => {
-            if (err.status === 404) {
-                const userSetting = new UserSetting(this.userId);
-                this.userSetting = userSetting;
-            }
-        });
     }
 
     getRecentResources() {
         if (this.userSetting) {
             return of(this.userSetting);
         }
-        return this._diagnosticApiService.getUserSetting(this.userId).pipe(catchError(err => {
-            if (err.status === 404) {
-                const userSetting = new UserSetting(this.userId);
-                return of(userSetting);
-            }
-        }), map(userInfo => {
-            this.userSetting = userInfo;
-            return userInfo;
-        }));
+
+        return this._diagnosticApiService.getUserSetting(this.userId).pipe(
+            map(userSetting => {
+                this.userSetting = userSetting;
+                return userSetting;
+            })
+        );
     }
 
     updateRecentResource(recentResource: RecentResource) {
-        if (this.userSetting) {
-            const resources = this.addRecentResource(recentResource, this.userSetting.resources);
-            this.userSetting.resources = resources;
-            return this._diagnosticApiService.updateUserSetting(this.userSetting).pipe(map(setting => {
-                this.userSetting = setting;
-                return setting;
-            }));
-        } else {
-            this.getRecentResources().pipe(flatMap(userInfo => {
-                this.userSetting.resources = this.addRecentResource(recentResource, this.userSetting.resources);
-                return this._diagnosticApiService.updateUserSetting(this.userSetting).pipe(map(setting => {
-                    this.userSetting = setting;
-                    return setting;
-                }));
-            }));
-        }
+        this.updateUserSetting(recentResource, this.addRecentResource);
     }
 
-    private addRecentResource(newResource: RecentResource, recentResources: RecentResource[]) {
-        const res = [...recentResources];
-        const index = recentResources.findIndex(resource => resource.resourceUri.toLowerCase() === newResource.resourceUri.toLowerCase());
+    private updateUserSetting(item: any, fn: UpdateUserSettingCallBack) {
+        this.getRecentResources().pipe(catchError(error => {
+            if (error.status === 404) {
+                const userSetting = new UserSetting(this.userId);
+                return of(userSetting);
+            }
+            throw error;
+        })).subscribe(userSetting => {
+            const newUserSetting = fn(item, userSetting);
+            this._diagnosticApiService.updateUserSetting(newUserSetting).subscribe(setting => {
+                this.userSetting = setting;
+            })
+        });
+    }
+
+    private addRecentResource(newResource: RecentResource, userSetting: UserSetting) {
+        const newUserSetting = { ...userSetting };
+        const res = [...newUserSetting.resources];
+        const index = userSetting.resources.findIndex(resource => resource.resourceUri.toLowerCase() === newResource.resourceUri.toLowerCase());
         if (index >= 0) {
             res.splice(index, 1);
         } else if (res.length >= maxRecentResourceLength) {
             res.pop();
         }
         res.unshift(newResource);
-        return res;
+
+        newUserSetting.resources = res;
+        return newUserSetting;
     }
 }
+
+type UpdateUserSettingCallBack = (item: any, userSetting: UserSetting) => UserSetting
