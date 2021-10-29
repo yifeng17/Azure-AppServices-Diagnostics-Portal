13037dc6a62fa660171c117a9dcd3461e650af43
Darrel Donald <darreldonald@microsoft.com>
2021-10-29 16:21:23 -0700

    Darreldonald load code on init (#1318)
    * pull code from repo on init
    
    * pull code from repo on init
    
    * Update GithubClientService.cs
    
    * load default code on create detector

--- AngularApp/projects/applens/src/app/modules/dashboard/onboarding-flow/onboarding-flow.component.ts
+++ AngularApp/projects/applens/src/app/modules/dashboard/onboarding-flow/onboarding-flow.component.ts
@@ -289,7 +289,7 @@ export class OnboardingFlowComponent implements OnInit {
     this.displayBranch = this.Branch;
     this.diagnosticApiService.getDetectorCode(`${this.id}/${this.id}.csx`, this.Branch, this.resourceId).subscribe(x => {
       this.code = x;
-    })
+    });
     this.closeCallout();
   }
 
@@ -1261,7 +1261,14 @@ export class OnboardingFlowComponent implements OnInit {
 
     forkJoin(detectorFile, configuration, this.diagnosticApiService.getGists()).subscribe(res => {
       this.codeLoaded = true;
-      this.code = res[0];
+      if (this.detectorGraduation && !(this.mode == DevelopMode.Create)){
+        this.diagnosticApiService.getDetectorCode(`${this.id}/${this.id}.csx`, this.Branch, this.resourceId).subscribe(x => {
+          this.code = x;
+        }); 
+      }
+      else{
+        this.code = res[0];
+      }
       this.originalCode = this.code;
       if (res[1] !== null) {
         this.gists = Object.keys(this.configuration['dependencies']);
